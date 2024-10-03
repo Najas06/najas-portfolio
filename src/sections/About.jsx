a34aa2522c58632ad52311dd2efe378a51fed3d7
Najas06 <najasneju@gmail.com>
2024-10-03 23:26:46 +0530

    dev_day_05

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,15 @@
 import Globe from "react-globe.gl";
 import Button from "../components/Button";
+import { useState } from "react";
 const About = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+  const handleCopy = ()=>{
+    navigator.clipboard.writeText('najasneju@gmail.com')
+    setHasCopied(true)
+    setTimeout(()=>{
+      setHasCopied(false)
+    },2000)
+  }
   return (
     <section className="c-space my-20">
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full ">
@@ -54,15 +63,48 @@ const About = () => {
               <p className="grid-headtext">
                 I'm currently working on the following projects
               </p>
-              <p className="grid-subtext">I'm based in India, with remote work.</p>
-              <Button name={'Contact Me'} isBeam containerClass={`w-full mt-10`}/>
+              <p className="grid-subtext">
+                I'm based in India, with remote work.
+              </p>
+              <Button
+                name={"Contact Me"}
+                isBeam
+                containerClass={`w-full mt-10`}
+              />
             </div>
           </div>
         </div>
         <div className="xl:col-span-2 xl:row-span-3">
-        <div className="grid-container">
-        <img src="/assets/grid3.png" alt="grid-3" className="" />
+          <div className="grid-container">
+            <img
+              src="/assets/grid3.png"
+              alt="grid-3"
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-subtext">
+                I love to implement new ideas and convert Figma to Code. Coding
+                isn't just my profession, it's my passion.
+              </p>
+            </div>
+          </div>
         </div>
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="/assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-contain sm:object-top"
+            />
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}> 
+                <img src={hasCopied? '/assets/tick.svg' : '/assets/copy.svg'} alt="copy" />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">najasneju@gmail.com</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
